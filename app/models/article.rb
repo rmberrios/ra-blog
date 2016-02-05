3e3e08feee166eae06cc8497120a0e38118a98b1
Rommel Berrios <rommel.berrios93@gmail.com>
2016-02-05 04:24:01 +0000

    Add user and create 1 to many articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@ class Article < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   validates :title, presence: true,
                     length: { minimum: 5}
-                    
+  belongs_to :user              
  # mora validatios at http://guides.rubyonrails.org/active_record_validations.html                   
 end
